f35d73da53213b1c89780d69473b8f60a748db47
HK273 <80052947+HK273@users.noreply.github.com>
2021-07-09 10:24:21 +0100

    Create RGif.R

---
+++ RGif.R
@@ -0,0 +1,66 @@
+# %>%  allows functions to be chained ogether (piped) rather than using multiple brackets
+# https://www.datacamp.com/community/tutorials/pipe-r-tutorial
+# Next step conver to R Markdown
+
+#Install Relevant package
+install.packages("RODBC")
+install.packages("viridis")
+install.packages("ggplot2")
+install.packages("gganimate")
+install.packages("babynames")
+install.packages("viridis")
+install.packages("dplyr")
+install.packages("gifski")
+
+# libraries:
+library("RODBC")
+library(ggplot2)
+library(gganimate)
+library(babynames)
+library(hrbrthemes)
+library(viridis)
+library(dplyr)
+library(gifski)
+
+#More info on pacakge
+help(package = "RODBC")
+
+#Test connection
+bisqldev <- odbcConnect("Test")
+
+#Write query into a data frame
+MyQuery <- sqlQuery(bisqldev,
+                    "SELECT T.name
+	,SUM(T.N) AS n
+	,T.year
+FROM BI_Reporting_Dev.dbo.tbl_testhkappt AS T
+WHERE T.name IS NOT NULL
+GROUP BY T.year
+,T.name
+ORDER BY T.year ASC")
+
+#Created a table for this query as too slow to run whole thing in R -- Test OX Appt Table
+
+# View data
+head(MyQuery)
+
+# Plot
+p <- MyQuery %>%
+  ggplot( aes(x=year, y=n, group=name, color=name)) +
+  geom_line() +
+  geom_point() +
+  scale_color_viridis(discrete = TRUE) +
+  ggtitle("Oxleas Appointments") +
+  theme_ipsum() +
+  ylab("Number of Appts") +
+  transition_reveal(year)
+
+# Use the animate function to render as gives more flexibility in design
+# print(p) # Animation variable - p
+
+
+# Render Gif - this can be a bit slow
+animate(p, duration = 10, fps = 20, width = 800, height = 489, renderer = gifski_renderer())
+
+# Safe Gif - Reccommend opening in browser to see full animation
+anim_save(filename = "H:/R/ox-appts.gif", animation = last_animation())
